54708ce41824e3bffefd8ad49bd94b58cee284ac
Sailesh Simhadri <Sailesh@localhost.localdomain>
2015-12-31 13:21:25 -0500

    Created Calcutor window.

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,4 +1,17 @@
+import javax.swing.JFrame;
 
-public class Calculator {
+public class Calculator extends JFrame {
+	
+	public Calculator(){
+		createCalculator();
+	}
+	
+	
+	public void createCalculator(){
+		setTitle("Calculator");
+		setSize(500,600);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setVisible(true);
+	}
 
 }
